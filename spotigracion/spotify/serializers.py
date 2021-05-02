1886bce40e272e53dfbc014c91f0a0605a252046
Javier Bizama <javierbizama@Javiers-MacBook-Pro.local>
2021-05-02 12:16:21 -0400

    start over

--- spotigracion/spotify/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers 
-from .models import Album, Artista, Cancion
-
-class ArtistaSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta: 
-        model = Artista
-        fields = ('name', 'age')
